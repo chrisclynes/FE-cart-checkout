f1909c728b7b1564fadb1d2e88b8a177ff9d274e
Chris Clynes <chrisclynes@live.com>
2022-10-11 16:10:49 -0400

    first commit, issue with react version and typescript, typescript components converted to jsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="my-4 text-center">Happy Coding!</h1>
+    </div>
+  );
+}
